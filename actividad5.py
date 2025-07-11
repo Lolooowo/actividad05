ce4cc4ed662a626b36ba91822e67f31643f010ec
ALUMNO <julio.taracena132@gmail.com>
2025-07-11 10:10:06 -0600

    case 1 y 2 creados

--- actividad5.py
+++ actividad5.py
@@ -4,3 +4,27 @@ class Estudiante:
         self.carne = carne
         self.carrera = carrera
         self.nota = nota
+    def informacion(self):
+        print(f"El estudiante {self.nombre} de la carrera: {self.carrera} que se identifica con el carné: {self.carne} su nota es : {self.nota}")
+Estudiantes = []
+while True:
+    print("1. Ingresar a un nuevo estudiante")
+    print("2. Buscar estudiante por su carné")
+    print("3. Promedio de todos los estudiantes ingresados")
+    print("4. SALIR")
+    opcion = int(input("Seleccione una opcion: "))
+    match opcion:
+        case 1:
+            nombre = input("Ingrese el nombre: ")
+            carne = input("Ingrese el carne: ")
+            carrera = input("Ingrese el carrera: ")
+            nota = input("Ingrese el nota final del estudiante: ")
+            nuevoEstudiante = Estudiante(nombre, carne, carrera, nota)
+            Estudiantes.append(nuevoEstudiante)
+            break
+        case 2:
+            buscarEstudiante = input("Ingrese el carné del estudiante que desea buscar: ")
+            for estudiante in Estudiantes:
+                if buscarEstudiante == estudiante.carne:
+                    estudiante.informacion()
+            break
