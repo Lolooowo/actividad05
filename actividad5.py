641bcfe95f1e84e1fa1d36cc4c20f5fd615f6aa6
ALUMNO <julio.taracena132@gmail.com>
2025-07-11 10:03:06 -0600

    creacion de la clase

---
+++ actividad5.py
@@ -0,0 +1,6 @@
+class Estudiante:
+    def __init__(self, nombre, carne, carrera, nota):
+        self.nombre = nombre
+        self.carne = carne
+        self.carrera = carrera
+        self.nota = nota
